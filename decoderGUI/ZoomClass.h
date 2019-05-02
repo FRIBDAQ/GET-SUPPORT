c9060bd93227214ace279fdb95d615a92aa08b79
Cerizza, Giordano <cerizza@nscl.msu.edu>
2019-05-02 10:20:33 -0400

    new Zoom class

--- decoderGUI/ZoomClass.h
+++ decoderGUI/ZoomClass.h
@@ -12,41 +12,72 @@ public:
   ZoomClass();
   ~ZoomClass();
   static void ZoomChannel(int tbs, int adc);
-
+  
 };
 
 inline ZoomClass::ZoomClass() {}
 inline ZoomClass::~ZoomClass() {}
 
-inline void ZoomClass::ZoomChannel(int tbs, int adc) {
+inline void ZoomClass::ZoomChannel(int tbs, int adc)
+//inline void ZoomClass::ZoomChannel(Int_t event, Int_t x, Int_t y, TObject *selected)
+{
+  //  int event = gPad->GetEvent();
+  //  if (event != 11) return;
+
+  TCanvas* current = gPad->GetCanvas();
+  std::string cname = current->GetName();
 
   TObject *select = gPad->GetSelected();
-  if(!select) return;
-  if (!select->InheritsFrom(TH1::Class())) {
-    gPad->SetUniqueID(0);
-    return;
-  }
-  TH1 *h = (TH1*)select;
+  if (!select) return;
   gPad->GetCanvas()->FeedbackMode(kTRUE);
 
-  //create or set the new canvas c2
-  TVirtualPad *padsav = gPad;
-  TCanvas *c2 = (TCanvas*)gROOT->GetListOfCanvases()->FindObject("c2");
-  if(c2) delete c2->GetPrimitive("");
-  else   c2 = new TCanvas("c2","",710,10,700,500);
-  c2->SetGrid();
-  c2->cd();
+  if (!select->InheritsFrom(TPad::Class())) return;
+  TPad *pad = (TPad*)select;    
+  std::string pname = pad->GetName();
 
-  TH1D* htmp = (TH1D*)h->Clone();
-  htmp->SetBins(tbs, 0, tbs-1);
-  htmp->SetStats(0);
-  htmp->SetLineColor(2);
-  htmp->SetLineWidth(3);
-  htmp->SetMinimum(0);
-  htmp->SetMaximum(adc);
-  htmp->Draw("histo");
-  c2->Update();
-  padsav->cd();
+  int px = pad->GetEventX();
+  int py = pad->GetEventY();
+  double xd = pad->AbsPixeltoX(px);
+  double yd = pad->AbsPixeltoY(py);
+  float x = pad->PadtoX(xd);
+  float y = pad->PadtoY(yd);
+  TPad *ppad = current->Pick(px, py, 0);
+
+  /*
+  if (cname != pname){
+    std::cout << "Pad name: " << pname << std::endl;
+    std::cout << px << " " << py << " " << ppad->GetName() << std::endl;
+    std::cout << "AbsPixeltoX " << xd << " " << yd << std::endl;
+    std::cout << "PadtoX " << x << " " << y << std::endl;
+  }
+  */
+  
+  TH1D* htmp = new TH1D;
+  
+  TObject *obj;
+  TIter next(pad->GetListOfPrimitives());
+  if (cname != pname){
+    while ((obj = next())) {
+      if (obj->InheritsFrom(TH1::Class())) {
+	TH1* h = (TH1*)obj;
+	htmp = (TH1D*)h->Clone();
+      }
+    }
+
+    TCanvas *c = (TCanvas*)gROOT->GetListOfCanvases()->FindObject("c");
+    if(c) delete c->GetPrimitive("");
+    else c = new TCanvas("c","",700,500);
+    c->SetGrid();
+    c->cd();
+    htmp->SetBins(tbs, 0, tbs-1);
+    htmp->SetStats(0);
+    htmp->SetLineColor(2);
+    htmp->SetLineWidth(3);
+    htmp->SetMinimum(0);
+    htmp->SetMaximum(adc);
+    htmp->Draw("histo");
+    c->Update();
+  }
 
 }
 
